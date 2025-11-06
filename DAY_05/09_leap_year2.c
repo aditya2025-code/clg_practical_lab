ad8563e3d0d62af76302e86c6bc61185dbf628f8
aditya2025-code <adi1020066002@gmail.com>
2025-11-06 19:40:08 +0530

    one line updated

--- DAY_05/09_leap_year2.c
+++ DAY_05/09_leap_year2.c
@@ -5,5 +5,5 @@ void main()
   int a;
   printf("Enter a year : ");
   scanf("%d", &a);
-  (a % 400 == 0) ? printf("%d is Leap Year", a) : (a % 4 == 0) ? (a % 100 != 0) ? printf("%d is Leap Year", a) : printf("%d is not Leap Year", a): printf("%d is Leap Year", a);
+  (a % 400 == 0) ? printf("%d is Leap Year", a) : (a % 4 == 0) ? (a % 100 != 0) ? printf("%d is Leap Year", a) : printf("%d is not Leap Year", a): printf("%d is not Leap Year", a);
 }
