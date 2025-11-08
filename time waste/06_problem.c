f5ad310fde5f9955c5b4021da3b5e35910d0682f
Aditya Das <adi1020066002@gmail.com>
2025-11-08 18:20:07 +0530

    initial commit

---
+++ time waste/06_problem.c
@@ -0,0 +1,28 @@
+// #include<stdio.h>
+// void main(){
+//   int i,j,n;
+//   printf("Enter the row: ");
+//   scanf("%d",&n);
+//   for(i=1;i<=n;i++)
+//   {
+//     for (j=n;j>=i;j--)
+//     {
+//       printf("%d",j);
+//     }
+//     printf("\n");
+//   }
+// }
+#include<stdio.h>
+void main(){
+  int i,j,n;
+  printf("Enter the row: ");
+  scanf("%d",&n);
+  for(i=n;i>=1;i--)
+  {
+    for (j=1;j<=i;j++)
+    {
+      printf("%d",j);
+    }
+    printf("\n");
+  }
+}
