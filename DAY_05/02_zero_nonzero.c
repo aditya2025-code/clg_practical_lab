1c7d0439f795be6d49b3d069f84434022bcafd28
Aditya Das <adi1020066002@gmail.com>
2025-11-08 17:24:02 +0530

    Deleted old DAY_ folders from youtube branch

--- DAY_05/02_zero_nonzero.c
+++
@@ -1,8 +0,0 @@
-//Write a program in c to check number is zero or non-zero using ternary oparator:
-#include<stdio.h>
-void main(){
-  int a;
-  printf("Enter a number : ");
-  scanf("%d",&a);
-  (a == 0)?printf("%d is Zero",a):printf("%d is non-Zero",a);
-}
