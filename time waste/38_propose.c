a933653c3a175e99762defb37fd6be3a71c8c011
Aditya Das <adi1020066002@gmail.com>
2025-12-25 12:50:35 +0530

    added emoji

--- time waste/38_propose.c
+++ time waste/38_propose.c
@@ -2,33 +2,39 @@
 #include <windows.h>
 int main()
 {
+  SetConsoleOutputCP(CP_UTF8);
   int n;
   char her_choise;
-  char if_no[] = "OK.....\nBTW thank you for your honest answer....\nWe will still be good classmate";
-  char if_yes[] = "....I was not expecting this\nI need some time to write more code for this answer....\nTill then please wait....";
-  char fprint[] = "Hii..I'm Aditya\nI'm very curious about you...\nBy any chance....\nAre you single??\nEnter Y for yes or N for no\n";
-  printf("Enter any non-zero number to run: ");
+  char print[]="Enter any non-zero number to run: ";
+  char fprint[] = "Hii..I'm Aditya\nI'm very curious about you😅...\nBy any chance....\nAre you single??\nEnter Y for yes or N for no\n";
+  char if_no[] = "🙃 OK.....\nBTW thank you for your honest answer....\nWe will still be good classmate🤗";
+  char if_yes[] = "😍😅....I was not expecting this\nI need some time to write more code for this answer....\nTill then please wait....🤗🙏🏻";
+  for (int i = 0; print[i] != '\0'; i++)
+    {
+      putchar(print[i]);
+      Sleep(25);
+    }
   scanf("%d", &n);
   if (n)
   {
 
-    for (int i = 0; fprint[i] != 0; i++)
+    for (int i = 0; fprint[i] != '\0'; i++)
     {
       putchar(fprint[i]);
-      Sleep(100);
+      Sleep(90);
     }
     scanf(" %c", &her_choise);
     if (her_choise == 'n' || her_choise == 'N')
-      for (int i = 0; if_no[i] != 0; i++)
+      for (int i = 0; if_no[i] != '\0'; i++)
       {
         putchar(if_no[i]);
-        Sleep(150);
+        Sleep(100);
       }
     else if (her_choise == 'y' || her_choise == 'Y')
-      for (int i = 0; if_yes[i] != 0; i++)
+      for (int i = 0; if_yes[i] != '\0'; i++)
       {
         putchar(if_yes[i]);
-        Sleep(150);
+        Sleep(100);
       }
     else
       printf("I think You enter a wrong input");
