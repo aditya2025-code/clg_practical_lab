ec9e712f8a5d8d8319b2effe2d78ea7e05d6265c
Aditya Das <adi1020066002@gmail.com>
2025-12-27 18:14:32 +0530

    Change the whole program

--- PDF programs/38_average_until_-1.c
+++ PDF programs/38_average_until_-1.c
@@ -3,48 +3,33 @@
 Write a C program that repeatedly reads integers from the user until -1 is entered, then prints the average of all numbers (excluding -1) using a loop.
 */
 
+ 
 #include <stdio.h>
 
-int main()
-{
-  int a[50];
-  float count = 0, sum = 0;
-  for (int i = 0; i < 50; i++)
-  {
-    printf("Enter number: ");
-    scanf("%d", &a[i]);
-    if (a[i] == -1)
-      break;
-    count++;
-    sum += a[i];
-  }
-  float avg = sum / count;
-  printf("TOTAL NUMBERS = %.0f\n", count);
-  printf("AVERAGE = %.2f", avg);
-  return 0;
+int main() {
+    
+    int a,sum=0,count=0,avg;
+    do
+    {
+        printf("Enter the number: ");
+        scanf("%d",&a);
+        if(a == -1)
+        break;
+        sum += a;
+        count++;
+    } while(a!=-1);
+    avg = sum / count;
+    printf("Average= %d",avg);
+    return 0;
 }
 
 // OUTPUT:
 /*
-Enter number: 5
-Enter number: 5
-Enter number: 5
-Enter number: 5
-Enter number: 5
-Enter number: 5
-Enter number: 6
-Enter number: 6
-Enter number: 6
-Enter number: 6
-Enter number: 7
-Enter number: 7
-Enter number: 9
-Enter number: 9
-Enter number: 9
-Enter number: 8
-Enter number: 8
-Enter number: 9
-Enter number: -1
-TOTAL NUMBERS = 18
-AVERAGE = 6.67
+Enter the number: 80
+Enter the number: 90
+Enter the number: 80
+Enter the number: 70
+Enter the number: 70
+Enter the number: -1
+Average= 78
 */
