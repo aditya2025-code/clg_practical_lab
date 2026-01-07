c48c67f9eb35e4ae18805bab640de04b5efcefcc
Aditya Das <adi1020066002@gmail.com>
2026-01-07 12:59:49 +0530

    modifi the linear program

--- PDF programs/27_linear_search.c
+++ PDF programs/27_linear_search.c
@@ -9,7 +9,7 @@ int main()
   int n;
   printf("Enter array size: ");
   scanf("%d", &n);
-  int a[n], search;
+  int a[n], search, count = 0;
   printf("Enter %d numbers: ", n);
   for (int i = 0; i < n; i++)
   {
@@ -20,11 +20,16 @@ int main()
   for (int i = 0; i < n; i++)
   {
     if (search == a[i])
+    {
       printf("%d is found at index %d\n", search, i);
+      count++;
+    }
   }
+  if (count == 0)
+    printf("%d is not found", search);
   return 0;
 }
-// OUTPUT:
+// OUTPUT1:
 /*
 Enter array size: 10
 Enter 10 numbers: 1 2 3 4 5 5 6 5 7 5
@@ -34,4 +39,12 @@ Now enter the searching element: 5
 5 is found at index 5
 5 is found at index 7
 5 is found at index 9
+*/
+// OUTPUT2:
+/*
+Enter array size: 5
+Enter 5 numbers: 5 2 5 25 1
+
+Now enter the searching element: 9
+9 is not found
 */
