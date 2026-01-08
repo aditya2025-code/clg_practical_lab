7ef405cf569f95080efc7cb04f63157bdc8c2e32
Aditya Das <adi1020066002@gmail.com>
2026-01-08 20:48:06 +0530

    program added

--- Question bank program/1st_5_marks_Question/36_sorting_algorithm_bubble_sort.c
+++ Question bank program/1st_5_marks_Question/36_sorting_algorithm_bubble_sort.c
@@ -13,15 +13,15 @@ int main()
     scanf("%d", &a[i]);
   printf("Ascending order = ");
 
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n - 1; i++)
   {
-    for (int j = 0; j < i + 1; j++)
+    for (int j = 0; j < (n - 1 - i); j++)
     {
-      if (a[j] > a[i + 1])
+      if (a[j] > a[j + 1])
       {
         swap = a[j];
-        a[j] = a[i + 1];
-        a[i + 1] = swap;
+        a[j] = a[j + 1];
+        a[j + 1] = swap;
       }
     }
   }
