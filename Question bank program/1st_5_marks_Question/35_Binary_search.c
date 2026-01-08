1d703e1ba4df40098558addec7d3f90f63c4dfd0
Aditya Das <adi1020066002@gmail.com>
2026-01-08 12:39:06 +0530

    program added

--- Question bank program/1st_5_marks_Question/35_Binary_search.c
+++ Question bank program/1st_5_marks_Question/35_Binary_search.c
@@ -3,23 +3,40 @@
 #include <stdio.h>
 int main()
 {
-  int a[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-  int low = 0, high = 8 - 1, mid, key;
-  printf("Enter the key element: ");
-  scanf("%d", &key);
-  while (low <= high)
+  int n;
+  printf("Enter Array size: ");
+  scanf("%d", &n);
+  if (n > 0)
   {
-    mid = (high + low) / 2;
-    if (a[mid] == key)
+    int a[n], low = 0, high = n - 1, mid, key;
+    printf("Enter Array element in Ascending order: ");
+    for (int i = 0; i < n; i++)
+      scanf("%d", &a[i]);
+    printf("Enter the key element: ");
+    scanf("%d", &key);
+    while (low <= high)
     {
-      printf("%d is found at index %d", key, mid);
-      return 0;
+      mid = (high + low) / 2;
+      if (a[mid] == key)
+      {
+        printf("%d is found at index %d", key, mid);
+        return 0;
+      }
+      else if (a[mid] < key)
+        low = mid + 1;
+      else
+        high = mid - 1;
     }
-    else if (a[mid] < key)
-      low = mid - 1;
-    else
-      high = mid + 1;
+    printf("%d is not Found", key);
   }
-  printf("%d is not Found");
+  else
+    printf("Array size should be Positive");
   return 0;
 }
+// OUTPUT:
+/*
+Enter Array size: 7
+Enter Array element in Ascending order: 11 55 68 95 105 111 199
+Enter the key element: 199
+199 is found at index 6
+*/
